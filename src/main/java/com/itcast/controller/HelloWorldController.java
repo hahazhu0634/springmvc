2da212f563ae9dacf17a274a0571059f025eb234
badboy <zhengjun.zhu@tiancity.com>
2019-12-27 09:49:37 +0800

    add msg hello

--- src/main/java/com/itcast/controller/HelloWorldController.java
+++ src/main/java/com/itcast/controller/HelloWorldController.java
@@ -10,6 +10,7 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String Hello(){
         System.out.println("success...");
+        System.out.println("hello");
         return "success";
     }
     }
